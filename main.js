dafc4a6cef467ef00df04940b5420de7de7a771d
Mudther Yassin <153933918+Mudther84@users.noreply.github.com>
2024-06-30 17:19:50 +0200

    Add files via upload
    new capital

---
+++ main.js
@@ -0,0 +1,93 @@
+let title = document.querySelector('.title');
+let turn = 'x';
+let squares = [];
+function end(num1,num2,num3){
+    title.innerHTML = '${squares[num1]} winner'; //console.log('done'); 
+    document.getElementById('item'+num1).style.background = 'black';
+    document.getElementById('item'+num2).style.background = 'black';
+    document.getElementById('item'+num3).style.background = 'black';
+    setInterval(function(){title.innerHTML += '.'},1000)
+    setTimeout(function(){location.reload()},4000)
+
+}
+function game(id)
+{
+    let elemment = document.getElementById(id);
+    if(turn === 'x' && elemment.innerHTML == '')
+        
+        
+            elemment.innerHTML = 'x'
+            turn = 'o';
+            title.innerHTML = 'o';
+        
+        else if(turn == 'o' && elemment.innerHTML == '')
+            elemment.innerHTML = 'o';
+            turn = 'x';
+            title.innerHTML = 'x';
+        
+        winner();
+}
+function winner()
+{
+for(let i = 1; i<10;i++)
+}
+    { 
+    squares[i] = document.getElementById('item' + i) .innerHTML;
+    }
+    if(squares[1] == squares[2] && squares[2] == squares[3] &&[1] != '')
+        //{
+            //title.innerHTML = '${squares[1]} winner'; //console.log('done'); 
+            //document.getElementById('item'+1).style.background = 'black';
+            //document.getElementById('item'+2).style.background = 'black';
+            //document.getElementById('item'+3).style.background = 'black';
+            //setInterval(function(){title.innerHTML += '.'},1000)
+            //setTimeout(function(){location.reload()},4000)
+        //}
+end(1,2,3);
+        {
+        else  if(squares[4] == squares[5] && squares[5] == squares[6] &&[5] != '');
+        }
+end(4,5,6);
+        {
+        else if(squares[7] == squares[8] && squares[8] == squares[9] &&[8] != '');
+        }
+end(7,8,9);
+        {
+        else     if(squares[1] == squares[4] && squares[4] == squares[7] &&[1] != '');
+        }
+end(1,4,7);
+        {
+         else    if(squares[2] == squares[5] && squares[5] == squares[8] &&[5] != '');
+        }
+end(2,5,8);
+        {
+        else     if(squares[3] == squares[6] && squares[6] == squares[9] &&[6] != '');
+        }
+end(3,6,9);
+        {
+        else   if(squares[1] == squares[5] && squares[5] == squares[9] &&[5] != '');
+        }
+end(1,5,9);
+        {
+        else     if(squares[3] == squares[5] && squares[5] == squares[7] &&[5] != '');
+        }
+        {
+end(3,5,7);
+        }
+//function game(id)
+//{
+    //let elemment = document.getElementById(id);
+    //if(turn === 'x' && elemment.innerHTML == '')
+        
+        
+            //elemment.innerHTML = 'x'
+            //turn = 'o';
+            //title.innerHTML = 'o';
+        
+        //else if(turn == 'o' && elemment.innerHTML == '')
+            //elemment.innerHTML = 'o';
+            //turn = 'x';
+            //title.innerHTML = 'x';
+        
+        //winner();
+//}
